5333af3cefca067af14674083e30754835b1e4e0
Arkadia <lajluch@gmail.com>
2022-02-22 18:57:39 +0100

    ruchy gracza wszystkie-poprawki

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -11,13 +11,12 @@ public class RockPaperScissors {
             String playerMove;
 
             while(true){
-                System.out.println("Make a move from: rock = 1, water = 2, air = 3, paper = 4, " +
-                        "sponge = 5, scissors = 6 or fire = 7");
+                System.out.println("Make a move from: rock, water, air, paper, sponge, scissors or fire");
                 playerMove = scan.nextLine();
-                if (playerMove.equals("1") || playerMove.equals("2") || playerMove.equals("3") ||
-                        playerMove.equals("4") || playerMove.equals("5") || playerMove.equals("6") ||
-                                playerMove.equals("7")){
-                                    break;
+                if (playerMove.equals("rock") || playerMove.equals("water") || playerMove.equals("air") ||
+                    playerMove.equals("paper") || playerMove.equals("sponge") || playerMove.equals("scissors") ||
+                    playerMove.equals("fire")){
+                    break;
                 }
                 System.out.println(playerMove + " is not a valid move.");
             }
@@ -26,108 +25,99 @@ public class RockPaperScissors {
 
             if (playerMove.equals(computerMove)) {
                 System.out.println("The game was a tie!");
-            }
-            else if (playerMove.equals("1")) {
-                if (computerMove.equals("2")) {
+            } else if (playerMove.equals("rock")) {
+                if (computerMove.equals("water")) {
                     System.out.println("Rock drowns in water. You lose!");}
-                else if(computerMove.equals("3")) {
+                else if(computerMove.equals("air")) {
                     System.out.println("Air erodes rock. You lose!");}
-                else if (computerMove.equals("4")) {
+                else if (computerMove.equals("paper")) {
                     System.out.println("Paper covers rock. You lose!");}
-                else if (computerMove.equals("5")) {
+                else if (computerMove.equals("sponge")) {
                     System.out.println("Rock destroys sponge. You win!");}
-                else if (computerMove.equals("6")) {
+                else if (computerMove.equals("scissors")) {
                     System.out.println("Rock crushes scissors. You win!");}
-                else if (computerMove.equals("7")) {
+                else if (computerMove.equals("fire")) {
                     System.out.println("Rock suffocates fire. You win!");}
-            }
-
-            else if (playerMove.equals("2")) {
-                if (computerMove.equals("1")) {
+            } else if (playerMove.equals("water")) {
+                if (computerMove.equals("rock")) {
                     System.out.println("Rock drowns in water. You win!");}
-                else if(computerMove.equals("3")) {
+                else if(computerMove.equals("air")) {
                     System.out.println("Water evaporates into air. You lose!");}
-                else if (computerMove.equals("4")) {
+                else if (computerMove.equals("paper")) {
                     System.out.println("Paper floats on the water. You lose!");}
-                else if (computerMove.equals("5")) {
+                else if (computerMove.equals("sponge")) {
                     System.out.println("Sponge absorbs water. You lose!");}
-                else if (computerMove.equals("6")) {
+                else if (computerMove.equals("scissors")) {
                     System.out.println("Scissors sinks into the water. You win!");}
-                else if (computerMove.equals("7")) {
+                else if (computerMove.equals("fire")) {
                     System.out.println("Water extinguish th fire. You win!");}
-            }
-
-            else if (playerMove.equals("3")) {
-                if (computerMove.equals("1")) {
+            } else if (playerMove.equals("air")) {
+                if (computerMove.equals("rock")) {
                     System.out.println("Air erodes rock. You win!");}
-                else if(computerMove.equals("2")) {
+                else if(computerMove.equals("water")) {
                     System.out.println("Water evaporates into aie. You win!");}
-                else if (computerMove.equals("4")) {
+                else if (computerMove.equals("paper")) {
                     System.out.println("Paper fly into the air. You lose!");}
-                else if (computerMove.equals("5")) {
+                else if (computerMove.equals("sponge")) {
                     System.out.println("Sponge turns into Spongebob. You lose!");}
-                else if (computerMove.equals("6")) {
+                else if (computerMove.equals("scissors")) {
                     System.out.println("Scissors cuts the air. You lose!");}
-                else if (computerMove.equals("7")) {
+                else if (computerMove.equals("fire")) {
                     System.out.println("Air quenches the fire. You win!");}
-            }
-            else if (playerMove.equals("4")) {
-                if (computerMove.equals("1")) {
+            } else if (playerMove.equals("paper")) {
+                if (computerMove.equals("rock")) {
                     System.out.println("Paper covers rock. You win!");
-                } else if (computerMove.equals("2")) {
+                } else if (computerMove.equals("water")) {
                     System.out.println("Paper floats on the water. You win!");
-                } else if (computerMove.equals("3")) {
+                } else if (computerMove.equals("air")) {
                     System.out.println("Paper fly into the air. You win!");
-                } else if (computerMove.equals("5")) {
+                } else if (computerMove.equals("sponge")) {
                     System.out.println("Sponge turns into Spongebob. You lose!");
-                } else if (computerMove.equals("6")) {
+                } else if (computerMove.equals("scissors")) {
                     System.out.println("Scissors cuts paper. You lose!");
-                } else if (computerMove.equals("7")) {
+                } else if (computerMove.equals("fire")) {
                     System.out.println("Fire burns paper. You lose!");
                 }
-            }
-            else if (playerMove.equals("5")) {
-                if (computerMove.equals("1")) {
+            } else if (playerMove.equals("sponge")) {
+                if (computerMove.equals("rock")) {
                     System.out.println("Rock destroys sponge. You lose!");
-                } else if (computerMove.equals("2")) {
+                } else if (computerMove.equals("water")) {
                     System.out.println("Sponge absorbs water. You win!");
-                } else if (computerMove.equals("3")) {
+                } else if (computerMove.equals("air")) {
                     System.out.println("Sponge turns into Spongebob. You win!");
-                } else if (computerMove.equals("4")) {
+                } else if (computerMove.equals("paper")) {
                     System.out.println("Sponge turns into Spongebob. You win!");
-                } else if (computerMove.equals("6")) {
+                } else if (computerMove.equals("scissors")) {
                     System.out.println("Scissors cuts sponge. You lose!");
-                } else if (computerMove.equals("7")) {
+                } else if (computerMove.equals("fire")) {
                     System.out.println("Fire burns sponge. You lose!");
                 }
-            }
-            else if (playerMove.equals("6")) {
-                if (computerMove.equals("1")) {
+            } else if (playerMove.equals("scissors")) {
+                if (computerMove.equals("rock")) {
                     System.out.println("Rock crushes scissors. You lose!");
-                } else if (computerMove.equals("2")) {
+                } else if (computerMove.equals("water")) {
                     System.out.println("Scissors sinks into the water. You lose!");
-                } else if (computerMove.equals("3")) {
+                } else if (computerMove.equals("air")) {
                     System.out.println("Scissors cuts the air. You win!");
-                } else if (computerMove.equals("4")) {
+                } else if (computerMove.equals("paper")) {
                     System.out.println("Scissors cuts paper. You win!");
-                } else if (computerMove.equals("5")) {
+                } else if (computerMove.equals("sponge")) {
                     System.out.println("Scissors cuts sponge. You win!");
-                } else if (computerMove.equals("7")) {
+                } else if (computerMove.equals("fire")) {
                     System.out.println("Fire melts scissors. You lose!");
                 }
-            }
-            else if (playerMove.equals("7")) {
-                if (computerMove.equals("1")) {
+            } else if (playerMove.equals("fire")) {
+                if (computerMove.equals("rock")) {
                     System.out.println("Rock suffocates fire. You lose!");
-                } else if (computerMove.equals("2")) {
+                } else if (computerMove.equals("water")) {
                     System.out.println("Water extinguish th fire. You lose!");
-                } else if (computerMove.equals("3")) {
+                } else if (computerMove.equals("air")) {
                     System.out.println("Air quenches the fire. You lose!");
-                } else if (computerMove.equals("4")) {
+                } else if (computerMove.equals("paper")) {
                     System.out.println("Fire burns paper. You win!");
-                } else if (computerMove.equals("5")) {
+                } else if (computerMove.equals("sponge")) {
                     System.out.println("Fire burns sponge. You win!");
-                } else if (computerMove.equals("6")) {
+                } else if (computerMove.equals("scissors")) {
                     System.out.println("Fire melts scissors. You win!");
                 }
             }
