78279d99f49b002838ffa6dbd6a38a70be4f1312
Adam Pusio <adam.pusio@outlook.com>
2022-02-24 09:29:43 +0100

    28-dodanie-remote-mysql - zmiana url

--- src/main/java/org/example/HibernateFactory.java
+++ src/main/java/org/example/HibernateFactory.java
@@ -8,9 +8,9 @@ import org.hibernate.cfg.Configuration;
 public class HibernateFactory {
     private Configuration getHibernateConfig() {
         Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://db4free.net:3306/projektgrupowy?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false");
-        configuration.setProperty("hibernate.connection.username", "javagdapol");
-        configuration.setProperty("hibernate.connection.password", "admin12345");
+        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://remotemysql.com:3306/0AAETZhCTg");
+        configuration.setProperty("hibernate.connection.username", "0AAETZhCTg");
+        configuration.setProperty("hibernate.connection.password", "RipnIQyjoR");
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
         configuration.setProperty("hibernate.hbm2ddl.auto", "update");
