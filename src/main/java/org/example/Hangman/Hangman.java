a1c6d20dc7474e8bc61bc67c3c101a030938633f
Marcin Lamek <fluffgit@gmail.com>
2022-02-25 09:52:37 +0100

    Player builder added

--- src/main/java/org/example/Hangman/Hangman.java
+++ src/main/java/org/example/Hangman/Hangman.java
@@ -6,53 +6,44 @@ import java.util.Set;
 
 public class Hangman {
 
-        public static final int WINNING_SCORE =3;
+    public static final int WINNING_SCORE = 3;
 
     public void start() {
-
-
-        Player player1 = new Player();
-        Player player2 = new Player();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("select first players name");
-        player1.setName(scanner.next());
-        player1.setScore(0);
-
+        Player player1 = new Player.PlayerBuilder(scanner.next()).score(0).build();
         System.out.println("select second players name");
-        player2.setName(scanner.next());
-        player2.setScore(0);
+        Player player2 = new Player.PlayerBuilder(scanner.next()).score(0).build();
 
         boolean endGame = false;
 
-        while(!endGame){
-            printScore(player1,player2);
-            runRuond(player1,scanner);
-            printScore(player1,player2);
-            runRuond(player2,scanner);
+        while (!endGame) {
+            printScore(player1, player2);
+            runRound(player1, scanner);
+            printScore(player1, player2);
+            runRound(player2, scanner);
 
-            if(player1.getScore()==WINNING_SCORE || player2.getScore()==WINNING_SCORE){
+            if (player1.getScore() == WINNING_SCORE || player2.getScore() == WINNING_SCORE) {
                 endGame = true;
-                if(player1.getScore()==player2.getScore()){
+                if (player1.getScore() == player2.getScore()) {
                     System.out.println("draw");
-                } else if (player1.getScore()==WINNING_SCORE) {
+                } else if (player1.getScore() == WINNING_SCORE) {
                     System.out.println("P1 wins");
                 } else {
                     System.out.println("P2 wins");
                 }
-
             }
         }
         scanner.close();
     }
 
     private void printScore(Player player1, Player player2) {
-        System.out.printf("P1 score: %d\n",player1.getScore());
-        System.out.printf("P2 score: %d\n",player2.getScore());
+        System.out.printf("P1 score: %d\n", player1.getScore());
+        System.out.printf("P2 score: %d\n", player2.getScore());
     }
 
-
-    public void runRuond(Player player,Scanner scanner){
+    public void runRound(Player player, Scanner scanner) {
         player.setChances(7);
         Set<Character> letters = new HashSet<>();
 
@@ -63,14 +54,14 @@ public class Hangman {
         String hashedWord = wordProccessor.hash(wordProccessor.getWordToArray());
 
         while (currentRound) {
-            System.out.printf("Grę zaczyna %s,ilość szans: %d, aktuana ilość punktów %d, wybierz literę\n",player.getName(),player.getChances(),player.getScore());
+            System.out.printf("Grę zaczyna %s,ilość szans: %d, aktuana ilość punktów %d, wybierz literę\n", player.getName(), player.getChances(), player.getScore());
             String s = scanner.next();
             char c = s.charAt(0);
 
             if (wordProccessor.check(wordProccessor.getWordToArray(), c)) {
                 hashedWord = wordProccessor.partialUnhash(wordProccessor.getWordToArray(), c, hashedWord);
                 System.out.println(hashedWord);
-            }else{
+            } else {
                 player.subtractChance();
                 System.out.println(player.getChances());
                 System.out.println(hashedWord);
@@ -80,20 +71,19 @@ public class Hangman {
             letters.add(c);
             System.out.println(letters);
 
-            if(player.getChances()==0){
-                System.out.printf("Przegrana! %s nie odgadł hasła, ilość punktów %d, pozostałe szane %d\n",player.getName(),player.getScore(), player.getChances());
+            if (player.getChances() == 0) {
+                System.out.printf("Przegrana! %s nie odgadł hasła, ilość punktów %d, pozostałe szane %d\n", player.getName(), player.getScore(), player.getChances());
                 currentRound = false;
             }
 
-            if(!wordProccessor.check(hashedWord.toCharArray(),'#')){
+            if (!wordProccessor.check(hashedWord.toCharArray(), '#')) {
                 player.increseScore();
-                System.out.printf("Wygrana! %s odgadł hasło, ilość punktów %d, pozostałe szane %d\n",player.getName(),player.getScore(), player.getChances());
+                System.out.printf("Wygrana! %s odgadł hasło, ilość punktów %d, pozostałe szane %d\n", player.getName(), player.getScore(), player.getChances());
                 currentRound = false;
             }
         }
 
     }
-
 
 
 }
--- src/main/java/org/example/Hangman/Player.java
+++ src/main/java/org/example/Hangman/Player.java
@@ -2,6 +2,8 @@ package org.example.Hangman;
 
 import lombok.Data;
 
+import java.awt.image.PackedColorModel;
+
 @Data
 public class Player {
 
@@ -22,4 +24,43 @@ public class Player {
     public int chancesReversed(){
        return this.chances -7;
     }
+
+    public static class PlayerBuilder{
+
+        private long id;
+        private String name;
+        private int score;
+        private int chanses;
+
+        public PlayerBuilder(String name){
+            this.name = name;
+        }
+
+        public PlayerBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public PlayerBuilder chances(int chanses){
+            this.chanses = chanses;
+            return this;
+        }
+
+        public PlayerBuilder score(int score){
+            this.score = score;
+            return this;
+        }
+
+        public Player build(){
+            Player player = new Player();
+            player.name = this.name;
+            player.chances = this.chanses;
+            player.score = this.score;
+            return player;
+        }
+
+
+    }
+
 }
+
