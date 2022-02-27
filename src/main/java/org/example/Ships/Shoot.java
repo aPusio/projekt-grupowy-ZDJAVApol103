30d03b0f008e7e2ae7ffdb479593408f2343df27
Kasuch <ksuchanowski@gmail.com>
2022-02-27 09:23:58 +0100

    Shoot class

--- src/main/java/org/example/Ships/Shoot.java
+++ src/main/java/org/example/Ships/Shoot.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 //Class responsible for shooting opposite player ship
 public class Shoot {
 
-    public static void Shoot() {
+    public static void Shoot(Board board) {
         int xCoordinate = 0;
         int yCoordinate = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please give your shot coordinates:");
         System.out.print("X = ");
-        scanner.nextInt();
+        xCoordinate = scanner.nextInt();
         System.out.println();
         System.out.println("Y = ");
-        scanner.nextInt();
+        yCoordinate = scanner.nextInt();
 
         while(xCoordinate < 0 || xCoordinate >= 9 || yCoordinate < 0 || yCoordinate > 9) {
             System.out.println("Target out of the board. Please provide correct coordinates");
         }
-        if (table[xCoordinate][yCoordinate] == true) {
+        if (board[xCoordinate][yCoordinate] == true) {
                 System.out.println("HIT!");
-                table[xCoordinate][yCoordinate] = false;
+                board[xCoordinate][yCoordinate] = false;
                 currentPlayerShipCount--;
                 // todo update SQL table Shots
                 // todo update SQL table Ships
